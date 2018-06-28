60e4d6a7dac81f1fe9ade1eb5a4d39972cb1afba
Dwivedi <dwivedv@amazon.com>
2018-06-27 21:21:25 -0700

    Fixed minor bug in test app

--- src/fitness/app/Application.java
+++ src/fitness/app/Application.java
@@ -28,7 +28,7 @@ class Application {
                 if (mode.equals("4")) {
                     System.out.println("Running test cases..");
                     TestApplication testApplication = new TestApplication();
-                    testApplication.singleTestNumber = 1;
+                    testApplication.singleTestNumber = 0;
                     testApplication.executeTests();
                     break;
                 }
